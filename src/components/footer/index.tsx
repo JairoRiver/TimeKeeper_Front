35ac341a47fdbb35c090e1b5ef5025815079deb6
JairoRiver <jairo.river@protonmail.com>
2024-06-25 23:57:00 +0200

    Add simple footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+    return (
+        <footer className="relative bg-blueGray-200 pt-8 pb-6 bg-slate-700">
+            <div className="container mx-auto px-4">
+                <div className="flex flex-wrap text-left lg:text-left">
+                    <div className="w-full lg:w-6/12 px-4">
+                        <h4 className="text-xl fonat-semibold text-blueGray-700">Estamos en contacto</h4>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
